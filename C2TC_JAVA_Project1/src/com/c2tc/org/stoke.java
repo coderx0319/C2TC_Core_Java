4ecec0f75c942320d261cccba351e21b2ffef70f
Shantanu Vedante <vu1f2122110@pvppcoe.ac.in>
2024-04-01 14:41:00 +0530

    Second Commit

--- C2TC_JAVA_Project1/src/com/c2tc/org/stoke.java
+++ C2TC_JAVA_Project1/src/com/c2tc/org/stoke.java
@@ -3,7 +3,7 @@ package com.c2tc.org;
 public class stoke {
 	
 	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, Shrushti!"); 
 	}
 
 }
